ae92f5ae197792de11d154505a11990f757ed821
Diiegor98 <diiegor98@gmail.com>
2023-12-18 17:24:02 -0300

    calculadora

---
+++ Calculadora.js
@@ -0,0 +1,19 @@
+/* Comienza con mayúscula pq es una clase*/
+
+class Calculadora {
+    sumar(num1, num2){
+        return num1 + num2
+    }
+
+    restar(num1, num2){
+        return num1 - num2
+    }
+    
+    multiplicar(num1, num2){
+        return num1 * num2
+    }
+
+    dividir(num1, num2){
+        return num1 / num2
+    }
+}
---
+++ app.js
@@ -0,0 +1,16 @@
+/* Seleccionamos los nodos HMTL */
+const displayValorAnterior = document.getElementById("valor-anterior");
+const displayValorActual = document.getElementById("valor-actual");
+const btnNumeros = document.querySelectorAll(".numero");
+const btnOperadores = document.querySelectorAll(".operador");
+
+const display = new Display(displayValorAnterior, displayValorActual);
+/* Interacción con los botones */
+
+btnNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+btnOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
